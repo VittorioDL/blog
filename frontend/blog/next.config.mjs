cc0a5cd125e5ff878b0593bbe01e796cc540fa4e
Vittorio De Lucia <victor03.VDL@gmail.com>
2025-04-28 09:01:12 +0200

    image remote patterns

--- frontend/blog/next.config.mjs
+++ frontend/blog/next.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["127.0.0.1"],
+      remotePatterns: [
+        // For local development
+        {
+          protocol: 'http',
+          hostname: 'localhost',
+          port: '1338',
+          pathname: '/uploads/**',
+        },
+        // For production
+        // {
+        //   protocol: 'https',
+        //   hostname: 'your-production-domain.com',
+        //   pathname: '/uploads/**',
+        // }
+      ],
     },
-};
-
-export default nextConfig;
+  }
+  
+  export default nextConfig
